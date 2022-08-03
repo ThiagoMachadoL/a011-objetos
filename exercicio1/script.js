aea34403aab69ba187c45ac04038a28406dbb900
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-08-02 21:29:44 -0300

    Exercicios - Aula11

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,10 +1,19 @@
 const sacolao = []
-const itens = {
-    nome: 'Carol',
-    preco: 25,
-    disponibilidade: true,
+const item1 = {
+    nome: "uva",
+    preco: 8 ,
+    disponibilidade: "true",
 }
-
-sacolao.push(itens);
-
+const item2 = { 
+    nome: "maça",
+    preco: 2,
+    disponibilidade: "true",
+}
+const item3 = { 
+    nome: "banana",
+    preco: 5,
+    disponibilidade: "true",
+}
+sacolao.push(item1, item2, item3);
 console.log(sacolao);
+console.log(sacolao.length);
